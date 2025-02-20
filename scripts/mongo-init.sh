7fab0e4f274c3e9e3b5f95628f38b8ee7deb5cc6
Oleg <nikolaev.o@gmail.com>
2025-02-20 08:32:10 +0500

    Update mongo-init.sh

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -26,7 +26,7 @@ while (!rs.status().ok) { sleep(1000); }
 EOF
 
 echo "Инициализация шарда 2..."
-docker compose exec -T shard2-db1 mongosh --port 27018 <<EOF
+docker compose exec -it shard2-db1 mongosh --port 27018 <<EOF
 rs.initiate({
   _id: "shard2",
   members: [{ _id: 0, host: "shard2-db1:27018" }]
@@ -35,7 +35,7 @@ while (!rs.status().ok) { sleep(1000); }
 EOF
 
 echo "Добавление шардов в MongoS..."
-docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+docker compose exec -it mongos_router mongosh --port 27020 <<EOF
 sh.addShard("shard1-db1:27017");
 sh.addShard("shard2-db1:27018");
 use somedb;
@@ -44,24 +44,24 @@ sh.shardCollection("somedb.helloDoc", {"name": "hashed"});
 EOF
 
 echo "Наполнение базы тестовыми данными..."
-docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+docker compose exec -it mongos_router mongosh --port 27020 <<EOF
 use somedb;
 for (var i = 0; i < 1000; i++) db.helloDoc.insertOne({age: i, name: "ly" + i});
 print("Общее количество документов:", db.helloDoc.countDocuments());
 EOF
 
 echo "Вывод статуса шардинга..."
-docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+docker compose exec -it mongos_router mongosh --port 27020 <<EOF
 db.printShardingStatus();
 EOF
 
 echo "Вывод количества документов в каждом шарде..."
-docker compose exec -T shard1-db1 mongosh --port 27017 <<EOF
+docker compose exec -it shard1-db1 mongosh --port 27017 <<EOF
 use somedb;
 print("Документы в shard1:", db.helloDoc.countDocuments());
 EOF
 
-docker compose exec -T shard2-db1 mongosh --port 27018 <<EOF
+docker compose exec -it shard2-db1 mongosh --port 27018 <<EOF
 use somedb;
 print("Документы в shard2:", db.helloDoc.countDocuments());
 EOF
